b6967847d831e36eeccb7ecfb67fbeac72ce0c41
Atul Rauthan <atulrauthan7@gmail.com>
2020-08-01 10:40:12 +0530

    Added API to call countries in dropdown

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select
+} from '@material-ui/core'
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }))
+          setCountries(countries)
+        })
+    }
+    getCountriesData()
+  }, [])
+
   return (
     <div className="app">
-      <h1>Lets build a COVID 19 tracker!</h1>
+      <div className="app_header">
+        <h1>COVID 19 TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries */}
+            {
+              countries.map(country => (
+                <MenuItem key={country.value} value={country.value}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+      {/* Info Box */}
+      {/* Info Box */}
+      {/* Info Box */}
+      {/* Table of cases and countries */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }
